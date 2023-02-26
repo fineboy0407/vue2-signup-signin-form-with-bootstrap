2d0e269dd67aedfcb8c9da1df96f37a8b80832f6
fineboy0407 <122990468+fineboy0407@users.noreply.github.com>
2023-02-25 21:05:26 -0500

    feat: add nav bar on the app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div class="vue-template">
+     <!-- Navigation -->
+     <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
+        <div class="container">
+            <a class="navbar-brand float-left" href="https://github.com" target="_blank">fineboy</a>
+        </div>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-primary" to="/">Sign up</router-link>
+          </li>
+        </ul>
+     </nav>
+
+     <!-- Main -->
+     <div class="App">
+        <div class="vertical-center">
+          <div class="inner-block">
+            <router-view/>
+          </div>
+        </div>
+     </div>
+   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+   name: '',
+   components: {
+     
+   },
+   mixins: [],
+   props: {
+     
+   },
+   data() {
+     return {
+       
+     }
+   },
+   computed: {
+     
+   },
+   watch: {
+     
+   },
+   mounted() {
+     
+   },
+   methods: {
+     
+   }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='' scoped>
 </style>
