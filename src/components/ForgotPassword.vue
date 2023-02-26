5416c84566dfc0cf44b1ea8887c271d862157082
fineboy0407 <122990468+fineboy0407@users.noreply.github.com>
2023-02-25 20:45:18 -0500

    feat: add Forgot Password Form

---
+++ src/components/ForgotPassword.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="vue-template">
+        <form>
+            <h3>Forgot Password</h3>
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control form-control-lg"/>
+            </div>
+            <button class="btn btn-dark btn-lg btn-block" type="submit">Reset Password</button>
+        </form>
+    </div>
+</template>
+<script>
+    export default{
+        data() {
+            return {}
+        }
+    }
+</script>
